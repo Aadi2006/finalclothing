6d35a4788c549e0ca49e9cc8755ad6355759c993
Aadi <70697442+Aadi2006@users.noreply.github.com>
2022-06-14 22:47:35 +0530

    Added little bit of sass to give file structure

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
+import "./categories.styles.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Men",
+    },
+    {
+      id: 5,
+      title: "Women",
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
+      {categories.map((category) => (
+        <div className="category-container" key={category.id}>
+          <div />
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop now</p>
+          </div>
         </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
